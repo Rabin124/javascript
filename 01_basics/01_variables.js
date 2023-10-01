b5548c8c74d65b6d2cd17bb2af91ce148b6e2d64
Rabin Adhikari <44668065+Rabin124@users.noreply.github.com>
2023-10-01 10:43:18 +0000

    datATYPES

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,13 @@
+const accountId = 2321091;
+let accountEmail = "rabin@gmail.com";
+var accountPassword = "12234";
+accountCity = "Chitwan";
+
+// accountId = 2;
+
+accountEmail = "rabinady123@gmail.com"
+accountPassword = "qwerty"
+accountCity = "Kathmandu"
+
+console.log(accountId);
+console.table([accountId, accountEmail, accountPassword, accountCity])
