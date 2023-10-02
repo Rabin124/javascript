628d1d37b8149d3f555fa6076181f27e6e876837
Rabin Adhikari <44668065+Rabin124@users.noreply.github.com>
2023-10-02 08:44:44 +0000

    datatypes

--- 01_basics/02_dataTypes.js
+++ 01_basics/02_dataTypes.js
@@ -2,3 +2,24 @@
 
 //alert (3+3); //we are using nodejs, not browser
 console.log(3+3);
+
+// DATATYPES
+
+let name = 'Rabin';
+let age = 24;
+let isPass = true;
+let isDefined;
+
+//string
+//boolean
+// Number
+// bigInt
+// null
+// undefined
+//  symbol => unique
+
+// OBJECT
+
+console.log(typeof "dd");
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
