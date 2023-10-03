67ba4a2f8286a77ab1c488968f52594b4c2c35a9
Rabin Adhikari <44668065+Rabin124@users.noreply.github.com>
2023-10-03 06:04:26 +0000

    conversion

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -22,5 +22,23 @@ let booleanIsLoggedIn = Boolean(isLoggedIn);
 
 let someNumber = 33
 let stringNumber = String(someNumber);
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+// **************** operations ****************
+
+let value = 3;
+let negValue = -value;
+console.log(negValue);
+
+// console.log(2 + 2);
+// console.log(2 - 2);
+// console.log(2 / 2);
+// console.log(2 * 2);
+// console.log(2 ** 2);
+// console.log(2 % 2);
+
+let str1 = "hello"
+let str2 = " Rabin"
+let str3 = str1 + str2;
+// console.log(str3);
